796d88aa2e45e5c339142efafa812e960fad5f3a
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-05-05 16:00:45 -0600

    feat: envolver el componente principal en SafeAreaView para mejorar la compatibilidad con dispositivos

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { NumericKeyboard } from '@/components/NumericKeyboard'
 import { TipSelector } from '@/components/TipSelector'
 import { useTipCalculator } from '@/hooks/useTipCalculator'
 import { StyleSheet, useWindowDimensions, View } from 'react-native'
-
+import { SafeAreaView } from 'react-native-safe-area-context'
 export default function Index() {
   const {
     amount,
@@ -29,54 +29,74 @@ export default function Index() {
   const isLandscape = width > height
 
   return (
-    <View style={[styles.container, isLandscape && styles.landscape]}>
-      <View style={styles.top}>
-        <Display
-          amount={amount}
-          tipAmount={tipAmount}
-          totalPerPerson={totalPerPerson}
-          isEditingTip={isEditingTip}
-          customTipInput={customTipInput}
-        />
+    <SafeAreaView style={styles.safe}>
+      <View style={[styles.container, isLandscape && styles.landscape]}>
+        <View style={styles.top}>
+          <Display
+            amount={amount}
+            tipAmount={tipAmount}
+            totalPerPerson={totalPerPerson}
+            isEditingTip={isEditingTip}
+            customTipInput={customTipInput}
+          />
 
-        <TipSelector
-          selected={customTip ?? tip}
-          onSelect={(v) => {
-            setCustomTip(null)
-            setIsEditingTip(false)
-            setTip(v)
-          }}
-          onCustom={activateCustomTip}
-          isCustomActive={isEditingTip}
-        />
+          <View style={styles.controls}>
+            <TipSelector
+              selected={customTip ?? tip}
+              onSelect={(v) => {
+                setCustomTip(null)
+                setIsEditingTip(false)
+                setTip(v)
+              }}
+              onCustom={activateCustomTip}
+              isCustomActive={isEditingTip}
+            />
 
-        <Counter
-          value={people}
-          onInc={incrementPeople}
-          onDec={decrementPeople}
-        />
+            <Counter
+              value={people}
+              onInc={incrementPeople}
+              onDec={decrementPeople}
+            />
+          </View>
+        </View>
+
+        <View style={styles.bottom}>
+          <NumericKeyboard onInput={buildNumber} onDelete={deleteLast} />
+        </View>
       </View>
-
-      <View style={styles.bottom}>
-        <NumericKeyboard onInput={buildNumber} onDelete={deleteLast} />
-      </View>
-    </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safe: {
     flex: 1,
     backgroundColor: '#121212'
   },
+
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 10
+  },
+
   landscape: {
     flexDirection: 'row'
   },
+
   top: {
     flex: 1,
-    padding: 10
+    marginTop: 20
   },
+
+  controls: {
+    marginTop: 20,
+    gap: 20
+  },
+
   bottom: {
-    flex: 1
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingBottom: 10
   }
 })
